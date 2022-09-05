71562909a9a234ba69ede75f6248c0b2071b22ea
RiyaShankar19 <103636655+RiyaShankar19@users.noreply.github.com>
2022-09-05 09:32:32 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+int  num;
+cout<<"Enter the an integer";
+cin >> num;
+if(num%2==0)
+{
+    cout<< num<< "It is Even";}
+    else {
+    cout<<num<<"It is odd";}
+
+    return 0;
+}
