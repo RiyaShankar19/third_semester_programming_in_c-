ac48fa2d423a846c1809a7e194b978da62f65e87
RiyaShankar19 <103636655+RiyaShankar19@users.noreply.github.com>
2022-09-05 09:26:23 +0530

    Add files via upload

---
+++ positive.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+   //Condition to check if the number is negative or positive 
+   int num=59;
+   if (num>0)
+   cout<< "The number is Positive";
+   else 
+   if(num<0)
+   cout<<"The number is negative";
+   else
+   cout<<"The number is zero";
+    return 0;
+}
