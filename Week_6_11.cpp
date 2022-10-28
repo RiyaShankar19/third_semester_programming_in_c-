a3d864f420ab9603ab6f8faf6f28d552373ba2b2
RiyaShankar19 <103636655+RiyaShankar19@users.noreply.github.com>
2022-10-28 16:20:11 +0530

    Update Week_6_11.cpp

--- Week_6_11.cpp
+++ Week_6_11.cpp
@@ -1,28 +1,64 @@
 #include<iostream>
 using namespace std;
-#define PI 3.1416
+class vol
+{
+    int l;
+    int b;
+    int r;
+    int h;
+public:
+vol(int a)
+    {
+        l=a;
+    }
+vol(int a,int b)
+    {
+        r=a;
+        h=b;
+  }
+vol(int a,int d,int c)
+    {
+        l=a;
+        b=d;
+        h=c;
+    }
+void cube()
+    {
+        cout<<"Volume of Cube:  "<<l*l*l << endl;
 
-// function prototypes
-float volume(float length, float breadth, float height);
-float volume(float radius);
-float volume(float radius, float height);
+    }
 
-int main(){
-	float cube_l = 40.0, cube_b = 30.0, cube_h = 10.0;
-	float sphere_r = 2.5;
-	float cylinder_r = 2.5, cylinder_h = 10.0;
-	cout<<"Volume of Cube ="<<volume(cube_l, cube_b, cube_h)<<endl;
-	cout<<"Volume of Sphere ="<<volume(sphere_r)<<endl;
-	cout<<"Volume of Cylinder ="<<volume(cylinder_r, cylinder_h)<<endl;
-	return 0;
-}
-// function defination
-float volume(float length, float breadth, float height){
-	return length * breadth * height;
-}
-float volume(float radius){
-	return (4.0/3.0) * PI * radius * radius *radius; 
-}
-float volume(float radius, float height){
-	return PI * radius *radius * height;
+void cyli() {
+
+            cout<<"Volume of Cylinder: "<<3.14*r*r*h <<endl;
+
+        }
+
+            void rec()
+
+            {
+
+                cout<<"Volume of Rect.Box:  "<<l*b*h << endl;
+
+            }
+
+};
+
+int main()
+
+{
+
+    vol v1(2);
+
+    v1.cube();
+
+    vol v2(3,4);
+
+    v2.cyli();
+
+    vol v3(1,2,3);
+
+    v3.rec();
+
+
 }
